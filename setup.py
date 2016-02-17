39a92be4365df338d86a49184705b23bdd486ad3
Andre Christoga <andrechristoga@gmail.com>
2016-02-17 23:08:22 +0700

    Testing tokyo city

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
 def main():
-	pass
+	# City	
+	cm = 0.375
+	input = raw_input("What's the city? ")
+
+	# Input action
+	if input == "tokyo":
+		tokyo = 6	
+		print "The time is ", tokyo * cm
 
 if __name__ == '__main__':
 	main()
