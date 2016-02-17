20b06599870626e595d32f26667fae02ced75a31
Andre Christoga <andrechristoga@gmail.com>
2016-02-17 23:09:19 +0700

    Structured script

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 
 def main():
 	# City	
-	cm = 0.375
+	tokyo = 6
+
+	# Input and the times number
+	times = 0.375
 	input = raw_input("What's the city? ")
 
 	# Input action
-	if input == "tokyo":
-		tokyo = 6	
-		print "The time is ", tokyo * cm
+	if input == "tokyo":			
+		print "The time is ", tokyo * times
 
 if __name__ == '__main__':
 	main()
