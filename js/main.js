9604eb91ae8a3d4d3f22de53c87d21270f84c816
Andre Christoga <andrechristoga@gmail.com>
2016-03-16 17:18:25 +0700

    External javascript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function tokyo() {
+  alert("The times is 2:25 minute");
+}
+function seoul() {
+  alert("The times is 2:07 minute");
+}
+function singapore() {
+  alert("The times is 57 minute");
+}
+function angkara() {
+  alert("The times is 3:38 minute");
+}
+function taipe() {
+  alert("The times is 1:05 minute");
+}
